5e79a37b8bab8b7fea8b5980a74069d1fff774e8
Kenichi Kawaguchi <sonatine@pontalk.com>
2025-09-16 19:46:51 +0900

    add timezoneinfo

--- generate_file.py
+++ generate_file.py
@@ -1,8 +1,11 @@
 # generate_file.py
 import datetime
+import zoneinfo
+
 
 def main():
-    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tz = zoneinfo.ZoneInfo("Asia/Tokyo")
+    now = datetime.datetime.now(tz).strftime("%Y%m%d-%H%M%S")
     filename = f"articles/generated_{now}.txt"
     with open(filename, "w", encoding="utf-8") as f:
         f.write(f"Hello! This file was generated at {now}\n")
